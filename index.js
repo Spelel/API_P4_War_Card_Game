6cd8447c9734f7c3935a1e3d57c411c0b5f4963d
Arek <arek-karczo.wp.pl@wp.pl>
2025-05-03 16:44:45 +0200

    added card holders, count for remaining card, info for name and who win each round.

--- index.js
+++ index.js
@@ -27,12 +27,63 @@
 
 //----------------------------------------------------------------------------------------
 btn.addEventListener("click", render)
+
+let cardData = []
+
 btn_draw.addEventListener("click", () => {
     fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
-        .then(data => console.log(data))
-
+        .then(data => {
+            console.log(data)
+            cardData = data
+            getImgToTheDom()
+            score(data.cards[0].value, data.cards[1].value)
+            cardNrRender(data.remaining)
+        })
 })
+
+
+function cardNrRender(count) {
+    document.getElementById("remainingCards").innerHTML =`
+    <p class="topText" >${count}</p>
+    `
+}
+
+
+function score(card1, card2) {
+
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+        "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1)
+    const card2ValueIndex = valueOptions.indexOf(card2)
+    
+    if (card1ValueIndex === card2ValueIndex) {
+        document.getElementById("winnerInfo").innerHTML =`
+        <h2>WAR!</h2>
+        `
+    } else if (card1ValueIndex > card2ValueIndex) {
+            document.getElementById("winnerInfo").innerHTML =`
+            <h2>Computer Wins!</h2>
+            `
+        } else {
+            document.getElementById("winnerInfo").innerHTML =`
+            <h2>Player Wins!</h2>
+            `
+        }
+}
+
+
+
+
+
+function getImgToTheDom() {
+    document.getElementById("cardOne").innerHTML = `
+    <img src="${cardData.cards[0].image}">
+    `
+    document.getElementById("cardTwo").innerHTML = `
+    <img src= "${cardData.cards[1].image}">
+    `
+}
 
 let deckId
 
@@ -40,11 +91,8 @@ function render (){
     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         deckId = data.deck_id
-        console.log(deckId)
-
-        btn_draw.style.display = "inline"
+        console.log("Game start")
     })
 }
 
